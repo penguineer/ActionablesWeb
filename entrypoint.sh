98875014b19eff3a7a93bd761e8a198fae2eaa60
Stefan Haun <tux@netz39.de>
2022-07-25 22:20:16 +0200

    :loud_sound: Add log entry for the ACTIONABLES_URL env

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ ROOT=/usr/local/apache2/htdocs/
 CONFIG_TMPL=$ROOT/config.json.template
 CONFIG=$ROOT/config.json
 
+echo "Actionables URL is set to $ACTIONABLES_URL"
+
 export EXISTING_VARS=$(printenv | awk -F= '{print $1}' | sed 's/^/\$/g' | paste -sd,);
 cat $CONFIG_TMPL | envsubst $EXISTING_VARS | tee $CONFIG
 
